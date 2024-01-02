817dd8d1c0d7d08182154681c40637c73f3abc60
kimjaer <y5g5776@naver.com>
2024-01-02 16:24:35 +0900

    scss 연결

--- src/Form.js
+++ src/Form.js
@@ -1,33 +1,33 @@
 import React from 'react'
-// import "./contact.scss";
-import "bootstrap/dist/css/bootstrap.css";
+import contact from "./contact.module.scss";
+import "bootstrap/dist/css/bootstrap.css"
 
 const Contact = () => {
   return (
-    <div className='contact bg-light py-5'>
+    <div className={`${contact.sfdf} bg-light py-5`}>
       <form action="#none" className='cominfo container'>
         <fieldset className='text-center'>필수 정보</fieldset>
-        <ul className='row'>
-          <li className='col-12'>
-            <input type="text" className='bg-white border' placeholder='회사 이름(필수)' required />
+        <ul className='row my-3'>
+          <li className='col-12 mb-2'>
+            <input type="text" className='bg-white border d-block w-100' placeholder='회사 이름(필수)' required />
           </li>
-          <li className='col-6'>
-            <input type="text" className='bg-white border' placeholder='담당자 성함(필수)' required />
+          <li className='col-6 mb-2'>
+            <input type="text" className='bg-white border  d-block w-100' placeholder='담당자 성함(필수)' required />
           </li>
-          <li className='col-6'>
-            <input type="text" className='bg-white border' placeholder='직책 (필수)' required />
-          </li>
-
-          <li className='col-6'>
-            <input type="text" className='bg-white border' placeholder='전화번호(필수)' required />
-          </li>
-          <li className='col-6'>
-            <input type="text" className='bg-white border' placeholder='이메일 주소(필수)' required />
+          <li className='col-6 mb-2'>
+            <input type="text" className='bg-white border  d-block w-100' placeholder='직책 (필수)' required />
           </li>
 
-          <li className='col-12'>
+          <li className='col-6 mb-2'>
+            <input type="text" className='bg-white border  d-block w-100' placeholder='전화번호(필수)' required />
+          </li>
+          <li className='col-6 mb-2'>
+            <input type="text" className='bg-white border  d-block w-100' placeholder='이메일 주소(필수)' required />
+          </li>
+
+          <li className='col-12 mb-2'>
             <strong className='d-block'>프로젝트 개요</strong>
-            <textarea name="" className='bg-white border' id="" cols="50" rows="10" placeholder='프로젝트와 관련된 기초 정보를 입력해 주세요.
+            <textarea name="" className='bg-white border  d-block w-100' id="" cols="50" rows="10" placeholder='프로젝트와 관련된 기초 정보를 입력해 주세요.
                 *만약 RFP가 있으시다면 아래 "파일 업로드"를 선택해 파일을 첨부하여 주십시오.'>
 
             </textarea>
