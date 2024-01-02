4fe8b141745d2d754333cf1ac66fa30da4b3153f
kimjaer <y5g5776@naver.com>
2024-01-02 11:35:08 +0900

    폼태그 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import { useRef } from 'react'
 function App() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const emailValue = emailRef.current.value;
+
+    const passwordValue = passwordRef.current.value;
+    console.log('Email:', emailValue);
+
+
+    console.log('Password:', passwordValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form_member'>
+      <form action="#none">
+        <h2>User Login</h2>
+        <ul>
+          <li>
+            <label htmlFor="email">
+              Email
+            </label><br />
+            <input type="text" required name='email' />
+          </li>
+          <li>
+            <label htmlFor="password">
+              Password
+            </label><br />
+            <input type="password" name='password' required />
+          </li>
+        </ul>
+        <button type='submit'>submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
